fd38ca46c6273a22341bc1a414fd04938f76f19a
Nick Wilson <nickw0584@icloud.com>
2019-09-19 22:27:51 -0500

    last failing try

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
-console.log("this is loaded");
+//console.log("this is loaded");
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: "14963efcc34e4bc8ac263c57fc24c7dd",
+    secret: "d0dd05d038d244ad8135b4965b3d572f"
 };
 
--- liri.js
+++ liri.js
@@ -3,12 +3,22 @@ const keys = require("./keys.js");
 const axios = require("axios");
 const fs = require("fs");
 const moment = require("moment")
+const Spotify = require('node-spotify-api');
+
+const spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
+
+
+
+
 
 const input = process.argv[2];
 const topic = process.argv[3];
 
-console.log(process.argv)
 
+// use the case words to run the document
 switch (input) {
   case "concert-this":
     bandInfo(topic);
@@ -27,55 +37,55 @@ switch (input) {
     break;
 }
 
-// uses spotify, omdb, imdb
-//use const for everything 
-// dont forget to use join
-//console.log(JSON.stringify(data, null, 2));
-//.join(" ")
+// // uses spotify, omdb, imdb
+// //use const for everything 
+// // dont forget to use join
+// //console.log(JSON.stringify(data, null, 2));
+// //.join(" ")
 
-//i need be able to get concert events//
-function bandInfo(query) {
-  console.log("hi")
-  const querlyUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
-  console.log(querlyUrl)
-  // // concert-this
-  axios.get(querlyUrl).then(
-    function (response) {
-      console.log(response.data[0].lineup[0])
-      //name
-      
-      console.log(response.data[0].venue.name)
-      
-      console.log(response.data[0])
+// //i need be able to get concert events//
+// function bandInfo(query) {
+//   console.log("hi")
+//   const querlyUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+//  console.log(querlyUrl)
+//   // // concert-this
+//   axios.get(querlyUrl).then(
+//     function (response) {
+//       console.log(response.data[0].lineup[0])
+//       //name
+//      console.log(response.data[0].venue.name)
+//        //venue
+//     console.log(response.data[0])
+//       //date
+//       let everything = [
+//        "Name: " + response.data[0].lineup + "\n",
+//         "Venue: " + response.data[0].venue.name + "\n",
+//         " Date: " + (moment(response.data[0].date).format("MM/DD/YYYY")),
+//      ].join("");
 
-      //venue
+//    })
+// }
 
-      //date
 
-      
-      // let everything = [
-      //   "Name: " + response.data.name + "\n",
-      //   "Venue: " + response.data.venue + "\n",
-      //   " Date: " + (moment(bandResponse.data[0].date).format("MM/DD/YYYY")),
-      // ].join("");
-      // console.log(JSON.stringify(data, null, 2));
-    })
-}
-// // get song results
-// //spotify to get all of these things
-// //spotify-this-song
-// //spotify-this-song '<song name here>'
-// const spotify = new spotify(keys.spotify);
-// spotify.request("https://api.spotify.com/v1/search" + song )
-// .then (function(response3){
-//     let everything3 =[
-//         "Artist :" + response3.data.Artist + "\n",
-//         "SongName:" + response3.data.SongName + "\n",
-//         "URL:"     + response3.data.URL    + "\n",
-//         "Album: "  + response3.data.Album  + "\n",
-//     ].join("");
-//     console.log(JSON.stringify(data, null, 2));
-// })
+// function songInfo() {
+//   spotify.search({ type: 'track', query: 'All the Small Things', limit: 5 }, function (err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+//     else {
+//           console.log(
+//             "artist:" + data.tracks.items[0].album.artist[0] + "\n",
+//             "songName:" + data.tracks.items[0].songName + "\n",
+//             "urlLink:" + data.tracks.items[0].urlLink + "\n",
+//             "album:" + data.tracks.items[0].album + "\n"
+//           ).join("");
+//     console.log(data);
+//           }
+//         });
+//       };
+
+  
+
 
 // //movie-this
 // //movie result based on the actor or movie name
@@ -83,76 +93,45 @@ function bandInfo(query) {
 // //movie-this "<movie name here>";
 // //ratings .0 to get;
 
-// const movie = function() {
-//     this.findMovie= function (movie) {
-// let queryUrl2 = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// axios.get(queryUrl2).then(
-//     function(response2) {
-//         let everything2 = [
-//             "title: " + response2.data.title + "\n",
-//             "year: " + response2.data.year + "\n",
-//             " imdbRating: " + response2.data.imdbRating.average + "\n",
-//             "rottenRating " + response2.data.rottenRating.average + "\n",
-//             "country: " + response2.data.country + "\n",
-//             "languages" + response2.data.languages[4] + "\n",
-//             "plot: " + response2.data.plot + "\n",
-//             "actors:" + response2.data.actors + "\n",
-//           ].join("");
+// function movieName() {
+// console.log(hi)
+//     this.findMovie= function (movieName) {
+// const queryUrl2 = "http://www.omdbapi.com/" + movieName + "?apikey=[trilogy]&";
+//     console.log(queryUrl2);
+//     }}
+//  axios.get(queryUrl).then(
+//     function(response) {
+//             "title: " + response.data[0].title + "\n",
+//             "year: " + (moment(response.data[0].date).format("MM/DD/YYYY")) + "\n",
+//             " imdbRating: " + response.data[0].imdbRating.average + "\n",
+//             "rottenRating " + response.data[0].rottenRating.average + "\n",
+//             "country: " + response.data[0].country + "\n",
+//             "languages" + response.data[0].languages[4] + "\n",
+//             "plot: " + response.data[0].plot + "\n",
+//             "actors:" + response.data[0].actors + "\n"
+//       }.join(""))
 
-//     then.function(response.everything2);{
+//     then.function(response.everything);{
 
-//         if (everything2.confirms)
-//     console.log(everything2)
+//         if (everything.confirms)
+//     console.log(everything)
 
 //     else {console.log(
 //     queryURL = term || "Mr.Nobody",
 //     axios.get("http://omdbapi.com/?apikey=triligy&limit=1&t=" + myQuery))}
 //     console.log(JSON.stringify(data, null, 2));
-//     }
-// // have it search by what it wants//
-// //do-what-it-says//
-// function whatInfo(){
-// fs.readFile("random.txt", "utf8", function(error, data) {
-//   if (error) {
-//     return console.log(error);
-//   }
-//   const output = data.split(",");
-//   for (var i = 0; i < output.length; i++) {
-//     console.log(output[i]);
-//   }
-//   console.log(JSON.stringify(data, null, 2));
-// });
-
-
-//function bandInfo(){
-   // let artist = "";
-    //for (var i = 5; i < input.length; i++) {
-
-       // if (i > 2 && i < input.length) {
-         // artist = artist + "+" + input[i];
-        //} else {
-        //  artist += input[i];
-       // }
-//};
-
-//function songInfo(){
-   // let song = "";
-   // for (var i = 5; i < input.length; i++) {
-
-       // if (i > 5 && i < input.length) {
-        //  song = song + "+" + input[i];
-        //} else {
-         // song += input[i];
-       // }
-//};
-
-//function movieInfo(){
-  //  let movieName = "";
-   // for (var i = 5; i < input.length; i++) {
-
-   // if (i > 5 && i < input.length) {
-   ///   movieName = movieName + "+" + input[i];
-   // } else {
-     // movieName += input[i];
-
-
+//   };
+// //  // have it search by what it wants//
+// // //do-what-it-says//
+ function whatInfo(){
+fs.readFile("random.txt", "utf8", function(error, data) {
+  if (error) {
+    
+const output = data.split(",");
+  for (var i = 0; i < output.length; i++) {
+   console.log(output[i]);
+ }
+  console.log(JSON.stringify(data, null, 2));
+  return console.log(error)
+}}
+);}
